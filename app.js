7dbdcf6eb9a0099f4b18ae6ce7fa0fd66df6511b
Enejo James <spec.en.james@gmail.com>
2018-02-23 01:44:25 +0100

    initial setup

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 'use strict';
 const express = require('express');
+const mainController = require('./controllers/mainController.js');
 
 let app = express();
 
 app.set('view engine', 'ejs');
+
+mainController(app);
 
 app.listen(3000, () => {
     console.log('Now listening on 3000');
